9163026243c6965392b7c0d9ae77d7272cdd609e
ks3825 <komalfaldu97218@gmail.com>
2024-11-16 11:20:13 -0500

    done

---
+++ Module-9/hello-world/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React JS</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React Logo" className="react-logo" />
+        <div className="hello-world">Hello World</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
